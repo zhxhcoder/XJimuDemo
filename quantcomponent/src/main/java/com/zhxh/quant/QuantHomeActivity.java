2a1fd36a83d621ed855864176f971c99fb56c1e6
zhxh <zhxhcoder@gmail.com>
2018-12-12 19:25:40 +0800

    量化产品

---
+++ quantcomponent/src/main/java/com/zhxh/quant/QuantHomeActivity.java
@@ -0,0 +1,13 @@
+package com.zhxh.quant;
+
+import com.xlab.core.app.BaseActivity;
+
+/**
+ * Created by zhxh on 2018/12/12
+ */
+public class QuantHomeActivity extends BaseActivity {
+    @Override
+    protected void setLayout() {
+        setContentView(R.layout.quant_home_activity);
+    }
+}
---
+++ sharecomponent/src/main/runalone/java/com.zhxh.share.runalone/application/ShareApplication.java
@@ -0,0 +1,16 @@
+package com.zhxh.share.runalone.application;
+
+import com.xlab.core.app.BaseApplication;
+
+/**
+ * Created by zhxh on 2018/8/16.
+ */
+
+public class ShareApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
