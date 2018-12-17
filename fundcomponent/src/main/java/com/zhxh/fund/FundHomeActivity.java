349965bb413b9bcd45b31c52ff5e8d8d2159cf47
zhxh <zhxhcoder@gmail.com>
2018-12-17 20:16:16 +0800

    基金产品

---
+++ fundcomponent/src/main/java/com/zhxh/fund/FundHomeActivity.java
@@ -0,0 +1,14 @@
+package com.zhxh.fund;
+
+import com.xlab.core.app.BaseActivity;
+import com.zhxh.quant.R;
+
+/**
+ * Created by zhxh on 2018/12/17
+ */
+public class FundHomeActivity extends BaseActivity {
+    @Override
+    protected void setLayout() {
+        setContentView(R.layout.fund_home_activity);
+    }
+}
---
+++ fundcomponent/src/main/runalone/java/com.zhxh.fund.runalone/application/FundApplication.java
@@ -0,0 +1,14 @@
+package com.zhxh.quant.runalone.application;
+
+import com.xlab.core.app.BaseApplication;
+
+/**
+ * Created by zhxh on 2018/12/17.
+ */
+
+public class FundApplication extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
